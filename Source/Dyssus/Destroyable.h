5188b679a0fef1f7ae86649eb61c2fa763a87c39
michead <countrymetal@live.it>
2015-05-08 09:46:24 +0200

    initial support for Grabbable and Destroyable interfaces

---
+++ Source/Dyssus/Destroyable.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Destroyable.generated.h"
+
+UCLASS()
+class DYSSUS_API ADestroyable : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ADestroyable();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaSeconds) override;
+
+	// Destroy the object
+	virtual void Destroy() override;
+
+	// Destroy with "collision" property the object  
+	virtual void Destroy(Collision_object collision) override;
+
+
+};
---
+++ Source/Dyssus/Grabbable.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Grabbable.generated.h"
+
+UCLASS()
+class DYSSUS_API AGrabbable : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AGrabbable();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaSeconds) override;
+
+	// Make the object be grabbed by the character 
+	virtual void Grab() override;
+
+
+};
