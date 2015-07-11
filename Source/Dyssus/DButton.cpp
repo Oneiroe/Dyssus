3e88002d6d6238fbe95a1b481172fc6855eb298f
michead <countrymetal@live.it>
2015-07-11 13:14:59 +0200

    fixed compilation errors

--- Source/Dyssus/DButton.cpp
+++ Source/Dyssus/DButton.cpp
@@ -7,23 +7,23 @@
 // Sets default values
 ADButton::ADButton()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-    
-    // The button has a box collider, which represents the volume for the overlap to press the button
-    BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
-    BoxComponent->OnComponentBeginOverlap.AddDynamic(this, &ADButton::OnBeginOverlap);
-    BoxComponent->OnComponentEndOverlap.AddDynamic(this, &ADButton::OnEndOverlap);
-    BoxComponent->bGenerateOverlapEvents = true;
-    
-    StaticMeshcomponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
-    SetActorEnableCollision(true);
-    
-    RootComponent = StaticMeshComponent;
-    BoxComponent->AttachParent = StaticMeshComponent;
-    
-    // The button is not pressed by default
-    SetPressed(false);
+
+	// The button has a box collider, which represents the volume for the overlap to press the button
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	BoxComponent->OnComponentBeginOverlap.AddDynamic(this, &ADButton::OnBeginOverlap);
+	BoxComponent->OnComponentEndOverlap.AddDynamic(this, &ADButton::OnEndOverlap);
+	BoxComponent->bGenerateOverlapEvents = true;
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+	SetActorEnableCollision(true);
+
+	RootComponent = StaticMeshComponent;
+	BoxComponent->AttachParent = StaticMeshComponent;
+
+	// The button is not pressed by default
+	SetPressed(false);
 }
 
 // Called when the game starts or when spawned
@@ -40,29 +40,58 @@ void ADButton::Tick(float DeltaTime)
 
 void ADButton::OnConstruction(const FTransform& Transform)
 {
-    SetPressed(isPressed);
+	SetPressed(isPressed);
 }
 
 void ADButton::OnBeginOverlap(class AActor* OtherActor,
-                              class UPrimitiveComponent* OtherComp,
-                              int32 OtherBodyIndex,
-                              bool bFromSweep,
-                              const FHitResult& SweepResult)
+class UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult)
 {
-    // TODO:
-    // If OtherActor is a DCharacter or a DCube, the button changes state to pressed (isPressed = true)
-    // Play OnPressSound
+	// TODO:
+	// If OtherActor is a DCharacter or a DCube, the button changes state to pressed (isPressed = true)
+	// Play OnPressSound
 }
 
-void ADBarrier::OnEndOverlap(class AActor * OtherActor,
-                             class UPrimitiveComponent* OtherComp,
-                             int32 OtherBodyIndex)
+void ADButton::OnEndOverlap(class AActor * OtherActor,
+class UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex)
 {
-    // TODO:
-    // Change the state to released (isPressed = false)
-    // Play OnReleaseSound
-    
-    // Needed for generating overlapping events
-    BoxComponent->SetCollisionEnabled(ECollisionEnabled::Type::QueryOnly);
+	// TODO:
+	// Change the state to released (isPressed = false)
+	// Play OnReleaseSound
+
+	// Needed for generating overlapping events
+	BoxComponent->SetCollisionEnabled(ECollisionEnabled::Type::QueryOnly);
 }
 
+void ADButton::SetPressed(bool newPressed)
+{
+	isPressed = newPressed;
+}
+
+bool ADButton::IsButtonPressed()
+{
+	return isPressed;
+}
+
+USoundBase* ADButton::GetOnReleaseSound()
+{
+	return onReleaseSound;
+}
+
+void ADButton::SetOnReleaseSound(USoundBase* newReleaseSound)
+{
+	onReleaseSound = newReleaseSound;
+}
+
+USoundBase* ADButton::GetOnPressSound()
+{
+	return onPressSound;
+}
+
+void ADButton::SetOnPressSound(USoundBase* newPressSound)
+{
+	onPressSound = newPressSound;
+}
--- Source/Dyssus/DButton.h
+++ Source/Dyssus/DButton.h
@@ -11,59 +11,67 @@ class DYSSUS_API ADButton : public AActor
 	GENERATED_BODY()
 
 private:
-    // This indicates whether the button is currently pressed (true) or released (false)
-    UPROPERTY(EditAnywhere, Category = State)
-    bool isPressed;
-    
-    // Sound played when the button is released
-    UPROPERTY(EditAnywhere, Category = Sounds)
-    USoundBase* onReleaseSound;
-    
-    // Sound played when the button is pressed
-    UPROPERTY(EditAnywhere, Category = Sounds)
-    USoundBase* onPressSound;
-	
-public:	
+	// This indicates whether the button is currently pressed (true) or released (false)
+	UPROPERTY(EditAnywhere, Category = State)
+		bool isPressed;
+
+	// Sound played when the button is released
+	UPROPERTY(EditAnywhere, Category = Sounds)
+		USoundBase* onReleaseSound;
+
+	// Sound played when the button is pressed
+	UPROPERTY(EditAnywhere, Category = Sounds)
+		USoundBase* onPressSound;
+
+public:
 	// Sets default values for this actor's properties
 	ADButton();
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
+
 	// Called every frame
 	virtual void Tick(float DeltaSeconds) override;
-    
-    // Returns whether the button is currently pressed (true) or released (false)
-    UFUNCTION(BlueprintCallable, Category = State)
-    bool IsPressed();
-    
-    UFUNCTION(BlueprintCallable, Category = State)
-    void SetPressed(bool newPressed);
-    
-    // Setters and getters for release sound
-    UFUNCTION(BlueprintCallable, Category = Sounds)
-    USoundBase* GetOnReleaseSound();
-    
-    UFUNCTION(BlueprintCallable, Category = Sounds)
-    void SetOnReleaseSound(USoundBase* onReleaseSound);
-    
-    // Setters and getters for press sound
-    UFUNCTION(BlueprintCallable, Category = Sounds)
-    USoundBase* GetOnPressSound();
-    
-    UFUNCTION(BlueprintCallable, Category = Sounds)
-    void SetOnPressSound(USoundBase* onPressSound);
-    
-    // Functions for collisions
-    UFUNCTION(BlueprintCallable, Category = Collision)
-    void OnBeginOverlap(class AActor* OtherActor,
-                        class UPrimitiveComponent* OtherComp,
-                        int32 OtherBodyIndex,
-                        bool bFromSweep,
-                        const FHitResult &SweepResult);
-    
-    UFUNCTION(BlueprintCallable, Category = Collision)
-    void OnEndOverlap(class AActor * OtherActor,
-                      class UPrimitiveComponent* OtherComp,
-                      int32 OtherBodyIndex);
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	// Returns whether the button is currently pressed (true) or released (false)
+	UFUNCTION(BlueprintCallable, Category = State)
+		bool IsButtonPressed();
+
+	UFUNCTION(BlueprintCallable, Category = State)
+		void SetPressed(bool newPressed);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	class UBoxComponent* BoxComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	class UStaticMeshComponent* StaticMeshComponent;
+
+	// Setters and getters for release sound
+	UFUNCTION(BlueprintCallable, Category = Sounds)
+		USoundBase* GetOnReleaseSound();
+
+	UFUNCTION(BlueprintCallable, Category = Sounds)
+		void SetOnReleaseSound(USoundBase* releaseSound);
+
+	// Setters and getters for press sound
+	UFUNCTION(BlueprintCallable, Category = Sounds)
+		USoundBase* GetOnPressSound();
+
+	UFUNCTION(BlueprintCallable, Category = Sounds)
+		void SetOnPressSound(USoundBase* pressSound);
+
+	// Functions for collisions
+	UFUNCTION(BlueprintCallable, Category = Collision)
+		void OnBeginOverlap(class AActor* OtherActor,
+	class UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult &SweepResult);
+
+	UFUNCTION(BlueprintCallable, Category = Collision)
+		void OnEndOverlap(class AActor * OtherActor,
+	class UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
 };
